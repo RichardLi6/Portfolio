2772d1d852e20de4b59e6f37893c62cdcb4d8447
Richard Li <rli108@my.bcit.ca>
2024-08-23 03:49:07 -0700

    Add section divider after intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -46,7 +46,7 @@ export default function Intro() {
             </motion.span>
         </div>
       </div>
-      <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity:0, y: 100}}
         animate={{ opacity:1, y: 0}}
       >
@@ -54,7 +54,7 @@ export default function Intro() {
         <span className="font-bold">BCIT CST student</span> with a background in {" "}
         <span className="font-bold">Health Sciences</span>  I enjoy
         using my knowledge in <span className="italic"> programming </span> and <span className="italic"> Health field </span>to contribute to the world.
-      </motion.p>
+      </motion.h1>
 
       <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial = {{ opacity: 0, y: 100}}
