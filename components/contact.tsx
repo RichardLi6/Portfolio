632f40984cb004c5484a5206fc82f351f721b9b9
Richard Li <rli108@my.bcit.ca>
2024-10-27 16:54:11 -0700

    Add partial styling to Contact page

---
+++ components/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+
+export default function Contact() {
+  return (
+    <section id="contact">
+        <SectionHeading> Contact me</SectionHeading>
+        <p> Please contact me directly at <a className="underline">richardli.72@hotmail.com</a> or through this form</p>
+    </section>
+  )
+}
