c5dc8afb44c4ac5f37cc0b4b881112849b99fd9d
Richard Li <rli108@my.bcit.ca>
2024-10-27 16:41:48 -0700

    Remove fixed height on webpage

--- components/experience.tsx
+++ components/experience.tsx
@@ -8,7 +8,8 @@ import { experiencesData } from '@/lib/data';
 
 export default function Experience() {
   return (
-     <section id="experience">
+     <section id="experience"
+      className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Experience</SectionHeading>
       <VerticalTimeline lineColor =""> 
         {experiencesData.map((item, index) => (
