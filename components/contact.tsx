ce28ffb9bf43aaa49c55affc2c1d8987c06d6434
Richard Li <rli108@my.bcit.ca>
2024-12-15 03:45:55 -0800

    css progress on contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { FaPaperPlane } from 'react-icons/fa'
 
 export default function Contact() {
   return (
-    <section id="contact">
+      <section id="contact" className="mb-20 sm:mb-28 w-[min(100%, 38rem)]"> {/* So we are choosing 100% if less than 38rem, when we shrink*/}
         <SectionHeading> Contact me</SectionHeading>
-        <p> Please contact me directly at <a className="underline">richardli.72@hotmail.com</a> or through this form</p>
+        <p className= "text-gray-700"> Please contact me directly at {" "}
+          <a className="underline" href="mailto:richardli.72@hotmail.com">
+            richardli.72@hotmail.com
+            </a> {" "}
+             or through this form
+             </p>
+
+        <form className = "mt-10">
+          <input className="h-14 rounded-lg borderBlack"
+          type="email" />
+          <textarea />
+          <button type="submit"> 
+            Submit <FaPaperPlane/> 
+            </button>
+        </form>
     </section>
   )
 }
