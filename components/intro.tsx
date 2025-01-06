47837e6695720dda168b7a35ad65234b509d9d8e
Richard Li <rli108@my.bcit.ca>
2025-01-06 01:53:48 -0800

    Fix active:scale not having spacing, causing animation to fail

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,18 +85,18 @@ export default function Intro() {
         className="opacity-70 group-hover:translate-x-1 transition"/> </Link>
 
         <a className ="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 \
-        transition cursor-pointer borderBlack " href="/Li_Richard_Resume.pdf" download> 
+        transition cursor-pointer borderBlack dark:bg-white/10" href="/Li_Richard_Resume.pdf" download> 
             Download CV <HiDownload
             className="opacity-60 group-hover:translate-y-1 transition"/>
         </a>
 
-        <a className ="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]active:scale-105 \
-        transition cursor-pointer borderBlack" href="https://www.linkedin.com/in/richard-li7/" target="_blank">
+        <a className ="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 \
+        transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60" href="https://www.linkedin.com/in/richard-li7/" target="_blank">
             <BsLinkedin />
         </a>
 
         <a className ="bg-white p-4 text-gray-700 hover:text-gray-950  flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 \
-        transition cursor-pointer borderBlack" href="https://github.com/RichardLi6" target="_blank">
+        transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60" href="https://github.com/RichardLi6" target="_blank">
             <FaGithubSquare />
         </a>
 
